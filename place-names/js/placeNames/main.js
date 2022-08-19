bb864fab6bc511d3d7de12814640c39f50efd953
Ben Shelton <bks-44@hotmail.com>
2022-08-19 10:43:49 -0500

    translation completed

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,12 +1,26 @@
 // Put your code here
 
 const names = [
-   "Nashville", "Hong Kong", "The back yard", "Earth",
-    "London", "The mall", "Ryman Auditorium", "The Great Wall of China"
+  "Nashville", "Hong Kong", "The back yard", "Earth",
+  "London", "The mall", "Ryman Auditorium", "The Great Wall of China"
 ];
 
 console.log("All Pace Names");
 
 for (const name of names) {
   console.log(name);
+}
+
+let filteredNames = []
+
+for (const name of names) {
+  if (name.startsWith("The")) {
+    filteredNames.push(name)
+  }
+}
+
+console.log("'The' PLace Names")
+
+for (const name of filteredNames) {
+  console.log(name)
 }
